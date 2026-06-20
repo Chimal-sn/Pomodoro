a2d17e0efc08281c0e9850e01bb457e30a4fbcd4
Sony <sonychimo10@hotmail.com>
2026-06-20 09:56:30 -0600

    Update Boton_siguiente en funcionamiento

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,15 @@ const listaCanciones = [
 
 ];
 
+let cancionActual = 0;
+
 const botonplay = document.getElementById('boton_play');
 const botonpausa = document.getElementById('boton_pausa');
+const boton_siguiente = document.getElementById('boton_siguiente')
 const audio = document.getElementById('audio_musica');
 
 botonplay.addEventListener('click', () => {
+    audio.src = listaCanciones[cancionActual].ruta;
     audio.play();
     botonplay.style.display = 'none';
     botonpausa.style.display = 'block';
@@ -25,4 +29,13 @@ botonpausa.addEventListener('click', () => {
 audio.addEventListener('ended', () => {
     botonplay.style.display = 'block';
     botonpausa.style.display = 'none';
+})
+
+boton_siguiente.addEventListener('click', () => {
+    cancionActual++;
+    if (cancionActual >= listaCanciones.length) {
+        cancionActual = 0;
+    }
+    audio.src = listaCanciones[cancionActual].ruta;
+    audio.play();
 })
